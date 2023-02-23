7cc397fef0d44271c7ce3d17d582da755866cbae
35kartal35 <mozdemir3545@gmail.com>
2023-02-23 20:10:52 +0300

    newCommit

--- src/component/ListTelephone.js
+++ src/component/ListTelephone.js
@@ -4,12 +4,33 @@ import urls from "../api/urls";
 import { useSelector,useDispatch } from "react-redux";
 import actionTypes from "../redux/action/actionTypes";
 import { Link } from "react-router-dom";
+import CustomModal from "./CustomModal";
 
 
 const ListTelephone=()=>{
-  const dispatch = useDispatch
+  const dispatch = useDispatch();
   const {telephonesState, groupsState} = useSelector((state)=>state)
-  
+  const [deleteTelephone,setDeleteTelephone] = useState("")
+  const [showDeleteModal,setShowDeleteModal] = useState(false);
+
+  const deleteMobil =(id)=>{
+    dispatch({type:actionTypes.telephoneaction.DELETE_TELEPHONES_START});
+    api.delete(`${urls.telephones}/${id}`)
+    .then((res)=>{
+      dispatch({
+        type:actionTypes.telephoneaction.DELETE_TELEPHONES_SUCCESS,
+        payload:id,
+      })
+    })
+    .catch((err)=>{
+      dispatch({
+        type:actionTypes.telephoneaction.DELETE_TELEPHONES_FAİL,
+        payload:"Kitap silinirken bir hata oldu"
+      });
+    });
+  };
+
+
     return(
         <div>
            <table className="container my-5">
@@ -34,12 +55,16 @@ const ListTelephone=()=>{
       <td>{telephone.Telnumber}</td>
       <td>{myTelephone.name}</td>
       <td>
-        <button className="generalbtn deletebtn" 
+        <button onClick={()=>{
+          setDeleteTelephone(telephone.id)
+          setShowDeleteModal(false);
+        }}
+         className="generalbtn deletebtn" 
         >
           SİL
           </button>
         <button className="generalBtn editBtn">GÜNCELLE</button>
-        <Link to={`/Detail/${telephone.id}`} className="generalBtn">Detay</Link>
+        <Link to={`/detail/${telephone.id}`} className="generalBtn">Detay</Link>
         
         </td>
     </tr>
@@ -48,8 +73,19 @@ const ListTelephone=()=>{
     
   </tbody>
 </table>
+{showDeleteModal === true && (
+  <CustomModal
+    title="Silme"
+    mesage="silmek istediğinizden emin misiniz?"
+    onCansel={()=>setShowDeleteModal(false)}
+    onConfirm={()=>{
+      deleteMobil(deleteTelephone)
+      setShowDeleteModal(false)
+    }}
+    />
+)}
         </div>
-    )
-}
+    );
+};
 
 export default ListTelephone;
--- src/page/Detail.js
+++ src/page/Detail.js
@@ -1,32 +1,28 @@
 import React, { useEffect, useState } from "react";
 import Header from "../component/Header";
-import { Link,useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import api from "../api/api";
 import urls from "../api/urls";
 
 const Detail =()=>{
-    const params=useParams();
-    const [myMobil,setMyMobil] = useState(null);
-    const [mobilCategory,setMobilCategory] = useState(null);
+    
 
-    /*telefonları Axios ile çekeceğiz http://localhost:3004/telephones/1*/
+    const [myMobil,setMyMobil] = useState(null);
+   
+    
 
     useEffect(()=>{
-        api.get(`${urls.telephones}/${params.telephoneId}`)
-        .then((restelephone)=>{
+        api.get(`${urls.telephones}`)
+        .then((restelephone)=> {
             console.log(restelephone.data);
             setMyMobil(restelephone.data);
-            api.get(`${urls.groups}/${restelephone.data.groupId}`)
-            .then((resGroup)=>{
-                console.log(resGroup.data);
-                setMobilCategory(resGroup.data);
-            })
-            .catch((err)=> {});
-        })
+       
+                    })
+   
         .catch((err)=>{});
     },[]);
 
-    if (myMobil === null || mobilCategory === null) return null;
+    if (myMobil === null ) return null;
 
     return(
         <div>
@@ -34,7 +30,7 @@ const Detail =()=>{
             <div className="container my-5">
                 <h2>Adı : {myMobil.name}</h2>
                 <h2>Telefon Numarası : {myMobil.Telnumber}</h2>
-                <h2>Grup : {mobilCategory.name}</h2>
+                
                 <Link to={"/"}>Geri</Link>
 
             </div>
